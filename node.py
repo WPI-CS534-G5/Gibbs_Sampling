efb73c65e0c62f14dda61c7c1a2ae628f78e899a
Unknown <hericflores@gmail.com>
2018-03-14 14:12:24 -0400

    Created Node skeleton

--- node.py
+++ node.py
@@ -1,8 +1,24 @@
 class Node(object):
     """Node object in graph"""
-    def __init__(self, arg):
+    def __init__(self, name, table, probabilities, parents=[], children=[]):
         super(Node, self).__init__()
-        self.arg = arg
+        self.children = children
+        self.parents = parents
 
-    def get_arg(self):
-        return self.arg
+        self.name = name
+        self.table = table
+        self.probabilities = probabilities
+         
+        self.markov_blanket = []
+    
+    def add_child(self, child):
+        return 
+
+    def add_parent(self, parent):
+        return
+
+    def calculate_probability(self):
+        return
+
+    def get_markov_blanket(self):
+        return
