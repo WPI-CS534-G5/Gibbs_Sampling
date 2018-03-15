61339491a8f5e5c556ce07182999b5ed0aecd4fa
nkalampalikis@github.com <nkalampalikis97@gmail.com>
2018-03-15 18:59:09 -0400

    It does not workgit add .!

--- node.py
+++ node.py
@@ -1,24 +1,43 @@
-class Node(object):
-    """Node object in graph"""
-    def __init__(self, name, table, probabilities, parents=[], children=[]):
+
+class Node:
+
+    parents = []
+    children = []
+    status = None
+    name = None
+    probability = []
+    markovBlanket = []
+
+    def __init__(self,parents,children, status,table,name,probability,markovBlanket):
         super(Node, self).__init__()
-        self.children = children
         self.parents = parents
-
-        self.name = name
+        self.children = children
+        self.status = status
         self.table = table
-        self.probabilities = probabilities
-         
-        self.markov_blanket = []
-    
-    def add_child(self, child):
-        return 
+        self.name = name
+        self.probability = probability
+        self.markovBlanket = markovBlanket
 
-    def add_parent(self, parent):
-        return
+    def AddParern(Node):
+        self.parents.append(Node)
 
-    def calculate_probability(self):
-        return
+    def AddChild(Node):
+        self.append(Node)
 
-    def get_markov_blanket(self):
-        return
+    def SetStatus(Status):
+        self.status = Status
+
+    def GetStatus():
+        return self.status
+
+    def CalculateProbability():
+
+
+
+    def SetMarkovBlanket():
+        self.markovBlanket.extend(self.parents) # Adds all the parents to the Markov Blanket
+        self.markovBlanket.extend(self.children) # Adds all the children to the Markov Blanket
+        for child in children: # Loops through all the children
+            for parent in child.parents: # Loops through all the parents of each child
+                if parent not in self.markovBlanket: # If any of those parents are not the Markov Blanket
+                    self.markovBlanket.append(parent) # It adds them to it
